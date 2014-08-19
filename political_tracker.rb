e09495a0bbcb8e32598ef00a88306e994c54a35a
keebz <keebz@keebz.com>
2014-08-19 16:05:10 -0700

    finished UI for admin menu

--- political_tracker.rb
+++ political_tracker.rb
@@ -18,11 +18,36 @@ def main
 		voter_menu
 	elsif selection == '2'
 		admin_menu
+	elsif selection == '$'
+		secret_menu
 	else
 		puts "Invalid input"
 		main
 	end
 end
+
+def secret_menu
+	puts "welcome special friend... I missed you."
+	puts "to create a new funder for a political representive press [1]"
+	puts "to see all of the funders and their representive puppets press [2]"
+	puts "to exit press [x]"
+	selection = gets.chomp.to_s
+	if selection == 'x'
+		puts "you saw nothing..."
+		sleep 1
+		main
+	elsif selection == '1'
+		add_funder
+		secret_menu
+	elsif selection == '2'
+		secret_list
+	else
+		puts "Invalid... WHO ARE YOU!?!?"
+		main
+	end			
+end
+
+
 
 def admin_menu
 	puts "Welcome 'ADMIN' Please enter password or type hint for password reminder:"
@@ -35,7 +60,7 @@ def admin_menu
 		puts "ADMIN confirmed..."
 		loop do
 			puts "To add a representative press [1]"
-			puts "To edit a representative press [2]"
+			puts "To delete a representative press [2]"
 			puts "To add a party press [3]"
 			puts "To exit to main menu press [m]"
 
@@ -81,7 +106,6 @@ def add_representative
 		else
 			state = State.find_by(name: user_state)
 		end
-
 	puts "Please enter the name of your new representatives party:"
 	list_parties
 	party = Party.find_by(name: gets.chomp.capitalize)
@@ -90,6 +114,24 @@ def add_representative
 	type = Type.create({name: gets.chomp.capitalize})
 	new_representative = Representative.create({name: name, state_id: state.id, party_id: party.id, type_id: type.id})
 	puts party.name + " " + type.name + " " + new_representative.name + " from the state of " + state.name + " has been added!"
+end
+
+def list_representatives
+	Representative.all.each_with_index do |representive, index|
+		puts (index + 1).to_s + ". " + Party.find_by(id: representive.party_id).name + " " + Type.find_by(id: representive.type_id).name + " " + representive.name + " From :" + State.find_by(id: representive.state_id).name
+	end
+end
+
+def edit_representative
+	list_representatives
+	puts "\n\n Select the number for the representative you would like to delete or press [x] to exit:"
+	selection = gets.chomp.to_s
+	if selection == 'x'
+		admin_menu
+	end
+	representive = Representative.all[selection.to_i - 1]
+	representive.destroy
+	puts "Deleted!"	
 end
 
 
