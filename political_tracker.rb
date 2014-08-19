3cf0e7306395906c269e411bb1207578aab9a192
keebz <keebz@keebz.com>
2014-08-19 14:36:22 -0700

    start working on the UI.

--- political_tracker.rb
+++ political_tracker.rb
@@ -4,3 +4,8 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))["development"])
 DB = PG.connect({:dbname => 'political_tracker_development'})
+
+def main
+	puts "Welcome"
+end
+main
