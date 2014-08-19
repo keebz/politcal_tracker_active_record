94f234af8a80584a70a3a91808fc67f82cc089ca
keebz <keebz@keebz.com>
2014-08-19 16:40:58 -0700

    added a call back to capitalize a states name.

--- lib/state.rb
+++ lib/state.rb
@@ -1,3 +1,10 @@
 class State < ActiveRecord::Base
 	has_many :representatives
+	before_save :capitalize_name
+
+private
+
+	def capitalize_name
+		self.name = self.name.capitalize
+	end
 end
--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -7,4 +7,8 @@ describe State do
     new_representative = Representative.create({name: "Mr. Smith", state_id: new_state.id})
     expect(new_state.representatives).to eq [new_representative]
   end
+    it 'converts the name to capitalize the first letter' do
+    new_state = State.create(:name => "oregon")
+    new_state.name.should eq "Oregon"
+  end
 end
